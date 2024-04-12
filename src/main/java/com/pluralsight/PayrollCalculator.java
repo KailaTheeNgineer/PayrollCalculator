03c936758c9d649d7b73b8bf423d14d1a989d989
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-04-12 11:27:08 -0400

    New Calculator

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+import java.util.*;
+/* initialising user input */
+
+public class PayrollCalculator {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+System.out.println("Welcome to Payroll Calculator");
+System.out.println("To begin, please enter your Name: ");
+       String name = myScanner.nextLine();
+
+        System.out.println("Please enter how many hours you work: ");
+        double hours = myScanner.nextDouble();
+
+        System.out.println("Please enter your pay rate: ");
+        double pay = myScanner.nextDouble();
+
+        double salary = hours * pay;
+        System.out.println("Thank you for using Payroll Calculator, " + name);
+        System.out.println("Your gross pay is: " + salary);
+
+
+
+
+
+
+
+
+    }
+}
